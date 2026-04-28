db315e6063f8ffeefe60b2e7924d49f547d068dc
herobrauni <braunnicolaj@gmail.com>
2026-04-28 07:53:06 +0200

    Fix NetBird SSH login path

--- modules/aspects/netbird.nix
+++ modules/aspects/netbird.nix
@@ -2,11 +2,21 @@
 {
   # Shared NetBird client aspect.
   den.aspects.netbird.nixos =
-    { options, ... }:
+    {
+      config,
+      options,
+      pkgs,
+      ...
+    }:
     lib.mkMerge [
       {
         services.netbird.enable = true;
         services.netbird.clients.default.config.ServerSSHAllowed = true;
+
+        # NetBird SSH shells call exec.LookPath("login") from the daemon.
+        # NixOS service PATHs do not include login by default; see:
+        # https://github.com/NixOS/nixpkgs/issues/505846
+        systemd.services.${config.services.netbird.clients.default.service.name}.path = [ pkgs.shadow ];
       }
 
       (lib.optionalAttrs (options.environment ? persistence) {
