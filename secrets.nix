512aec85cb6e6600af2768c2267e9fcf66f6fa7c
herobrauni <braunnicolaj@gmail.com>
2026-05-01 21:44:36 +0200

    fix: update onidel2 host key after first deploy

--- secrets.nix
+++ secrets.nix
@@ -31,7 +31,7 @@ let
   bero1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMzjGg0OUxUBcuAHsPFPk4FPYacWuR3wFBDF1T/k2Dox root@bero1";
   gc3 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGlcLQTDze6L2oCui8IWviis8RwwesEj9W6+ixVCsBaq root@gc3";
   onidel1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKmsHhIiXyk3gwe5yaBcssW0pI/MKbUBf6NcnvKcrWaJ root@onidel1";
-  onidel2 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA root@onidel2"; # placeholder
+  onidel2 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICn0cxrT+kUfgLzeAfMQ266QcVugrote7usipOw9Uqs5 root@onidel2";
   gc1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICcGHM8NWXQj22c1uBhMuAeI63De/4nqhwWto4bjIAlq root@gc1";
 
   personal = sshKeys.brauni;
