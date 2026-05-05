90e841057fd38325723eace788cafb2f8bcf925c
herobrauni <braunnicolaj@gmail.com>
2026-05-05 12:01:41 +0200

    fix(oracle1): update host key, rekey agenix secrets

--- secrets.nix
+++ secrets.nix
@@ -35,8 +35,8 @@ let
   terabit1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFbiH8mAhL5vwz8WawRUP/LdENxgySaLspe4SYgqoO4E root@terabit1";
   deluxhost2 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGBB6UhpN3obR5ulPbB3yPmUNJxoJg2GFEDYzLDgKOuc root@deluxhost2";
   hostsailor1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILR61W/XemRLXwydGknvEKd93u/6rBFfKNTRRVXCb7Kd root@hostsailor1";
-  nuyek1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA root@nuyek1"; # placeholder
-  oracle1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA root@oracle1"; # placeholder
+  nuyek1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA root@nuyek1"; # placeholder - fix after deploy
+  oracle1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKjXC3NSz8Gsjl9uYmHE/XevUM0dUpyOOsxk5j4mHxh+ root@oracle1";
   gc1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICcGHM8NWXQj22c1uBhMuAeI63De/4nqhwWto4bjIAlq root@gc1";
 
   personal = sshKeys.brauni;
@@ -56,7 +56,7 @@ let
     terabit1
     deluxhost2
     hostsailor1
-    nuyek1
+    # nuyek1  # placeholder - add after first deploy
     oracle1
   ];
 in
