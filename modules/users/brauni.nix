e952529de930a19f433aa70bde3d50ac080e7ba5
herobrauni <braunnicolaj@gmail.com>
2026-06-05 11:47:03 +0200

    feat(brauni): add starship config matching dotfiles

--- modules/users/brauni.nix
+++ modules/users/brauni.nix
@@ -280,6 +280,8 @@ in
           enableNushellIntegration = true;
         };
 
+        xdg.configFile."starship.toml".source = ./starship.toml;
+
         programs.starship = {
           enable = true;
           enableNushellIntegration = true;
